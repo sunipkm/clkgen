50072bfaf1f1f34727e2cd5a97a2c31277cfa9fd
Sunip K. Mukherjee <snuipkmukherjee@gmail.com>
2021-02-23 18:45:43 -0500

    Streamlined multi-os sections

--- src/timer_gen.c
+++ src/timer_gen.c
@@ -69,14 +69,15 @@ int initialize()
 
 size_t start_timer(unsigned long long int interval, time_handler handler, t_timer type, void *user_data)
 {
-#ifndef __APPLE__
     struct timer_node *new_node = NULL;
-    struct itimerspec new_value;
 
     new_node = (struct timer_node *)malloc(sizeof(struct timer_node));
 
     if (new_node == NULL)
         return 0;
+
+#ifndef __APPLE__
+    struct itimerspec new_value;
 
     new_node->callback = handler;
     new_node->user_data = user_data;
@@ -106,16 +107,7 @@ size_t start_timer(unsigned long long int interval, time_handler handler, t_time
     }
 
     timerfd_settime(new_node->fd, 0, &new_value, NULL);
-
-    /*Inserting the timer node into the list*/
-    new_node->next = g_head;
-    g_head = new_node;
-
-    return (size_t)new_node;
 #else
-    struct timer_node *new_node = (struct timer_node *) malloc(sizeof(struct timer_node));
-    if (new_node == NULL)
-        return (size_t) NULL;
     new_node->timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
     dispatch_source_set_event_handler(new_node->timer, ^{
       handler((size_t)new_node, user_data);
@@ -128,19 +120,20 @@ size_t start_timer(unsigned long long int interval, time_handler handler, t_time
     dispatch_source_set_timer(new_node->timer, start, interval, 0);
     dispatch_resume(new_node->timer);
     new_node->active = true;
-    /** Inserting the timer node into the list */
+#endif
+    /*Inserting the timer node into the list*/
     new_node->next = g_head;
     g_head = new_node;
-    return (size_t) new_node;
-#endif
+
+    return (size_t)new_node;
 }
 
 size_t update_timer(size_t timer_id, unsigned long long interval, t_timer type)
 {
-#ifndef __APPLE__
     struct timer_node *node = (struct timer_node *)timer_id;
     if (node == NULL) // on error, invalid timer ID
         return (size_t)NULL;
+#ifndef __APPLE__
     struct itimerspec new_value;
 
     new_value.it_value.tv_sec = interval / 1000000000;
@@ -158,60 +151,32 @@ size_t update_timer(size_t timer_id, unsigned long long interval, t_timer type)
     }
 
     timerfd_settime(node->fd, 0, &new_value, NULL);
-    return (size_t)node;
 #else
-    struct timer_node *node = (struct timer_node *)timer_id;
-    if (node == NULL) // on error, invalid timer ID
-        return (size_t)NULL;
     dispatch_time_t start = dispatch_time(DISPATCH_TIME_NOW, 0);
     dispatch_suspend(node->timer);
     node->active = false;
     dispatch_source_set_timer(node->timer, start, interval, 0);
     dispatch_resume(node->timer);
     node->active = true;
-    return (size_t) node;
 #endif // __APPLE__
+    return (size_t) node;
 }
 
 void stop_timer(size_t timer_id)
 {
-#ifndef __APPLE__
     struct timer_node *tmp = NULL;
     struct timer_node *node = (struct timer_node *)timer_id;
 
     if (node == NULL)
         return;
-
+#ifndef __APPLE__
     close(node->fd);
-
-    if (node == g_head)
-    {
-        g_head = g_head->next;
-    }
-    else
-    {
-
-        tmp = g_head;
-
-        while (tmp && tmp->next != node)
-            tmp = tmp->next;
-
-        if (tmp)
-        {
-            /*tmp->next can not be NULL here.*/
-            tmp->next = tmp->next->next;
-        }
-    }
-    if (node)
-        free(node);
 #else
-    struct timer_node *tmp = NULL;
-    struct timer_node *node = (struct timer_node *)timer_id;
-    if (node == NULL) // on error, invalid timer ID
-        return;
     if (node->active == false)
         dispatch_source_cancel(node->timer);
     node->active = false;
+#endif
+
     if (node == g_head)
     {
         g_head = g_head->next;
@@ -232,20 +197,16 @@ void stop_timer(size_t timer_id)
     }
     if (node)
         free(node);
-#endif
 }
 
 void finalize()
 {
-#ifndef __APPLE__
+
     while (g_head)
         stop_timer((size_t)g_head);
-
+#ifndef __APPLE__
     pthread_cancel(g_thread_id);
     pthread_join(g_thread_id, NULL);
-#else
-    while(g_head)
-        stop_timer((size_t) g_head);
 #endif
 }
 #ifndef __APPLE__
